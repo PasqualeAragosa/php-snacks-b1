f25af554090c2ec1394845e187f4fe1c5d0d0e42
Pasquale Aragosa <aragosap@gmail.com>
2022-11-28 12:26:12 +0100

    fix ex

--- snack_1/index.php
+++ snack_1/index.php
@@ -4,38 +4,38 @@
 
 // Olimpia Milano - Cantù | 55-60
 
-// $partite = [
-//     [
-//         "casa" => "Lakers",
-//         "ospite" => "Miami",
-//         "puntiCasa" => 56,
-//         "puntiOspite" => 71
-//     ],
-//     [
-//         "casa" => "Boston",
-//         "ospite" => "Chicago",
-//         "puntiCasa" => 87,
-//         "puntiOspite" => 43
-//     ],
-//     [
-//         "casa" => "New York",
-//         "ospite" => "Orlando",
-//         "puntiCasa" => 62,
-//         "puntiOspite" => 78
-//     ],
-// ];
-
-// foreach ($partite as $partita) {
-//     echo $partita . '<br>';
-// }
-
 $partite = [
-    'Lakers - Miami / 56-71',
-    'Boston - Chicago / 87-43',
-    'New Yorl - Orlando / 62-78'
+    [
+        "casa" => "Lakers",
+        "ospite" => "Miami",
+        "puntiCasa" => 56,
+        "puntiOspite" => 71
+    ],
+    [
+        "casa" => "Boston",
+        "ospite" => "Chicago",
+        "puntiCasa" => 87,
+        "puntiOspite" => 43
+    ],
+    [
+        "casa" => "New York",
+        "ospite" => "Orlando",
+        "puntiCasa" => 62,
+        "puntiOspite" => 78
+    ],
 ];
 
 foreach ($partite as $partita) {
+    echo $partita['casa'] . ' - ' . $partita['ospite'] . ' | ' . $partita['puntiCasa'] . '-' . $partita['puntiOspite'] . '<br>';
+}
 
-    echo $partita . '<br>';
-};
+// $partite = [
+//     'Lakers - Miami / 56-71',
+//     'Boston - Chicago / 87-43',
+//     'New Yorl - Orlando / 62-78'
+// ];
+
+// foreach ($partite as $partita) {
+
+//     echo $partita . '<br>';
+// };
